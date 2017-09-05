f59be1ef4411c0d2c3804f66ea686c09fbd5d407
zzy <zzyu1010@163.com>
2017-09-05 23:30:18 +0800

    加入多线程执行

--- snatch_prj/src/org/test/JsoupTest.java
+++ snatch_prj/src/org/test/JsoupTest.java
@@ -1,5 +1,8 @@
 package org.test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.text.SimpleDateFormat;
@@ -19,86 +22,112 @@ public class JsoupTest {
 	private static final String SITE_URL = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/index.html";
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("start:" + new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()));
-		PrintWriter writer = new PrintWriter("C:\\Users\\ZZY\\Desktop\\data.sql");
-		getProvince(writer);
-		writer.close();
-		System.out.println("end:" + new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()));
+		getProvince();
 	}
-	
+
 	/**
 	 * 获取省份
 	 */
-	private static void getProvince(PrintWriter writer) {
-			Document  doc = open(SITE_URL);
-			Elements links = doc.select("tr.provincetr").select("a");
-			RegionEntry province = null;
-			for (int i = 0; i < links.size(); i++) {
-				Element e = links.get(i);
-				province = new RegionEntry();
-				String href = e.attr("href"); // 14.html
-				String[] arr = href.split("\\.");
-				String name = e.text();
-				StringBuffer code = new StringBuffer(arr[0]);
-				while (code.length() < 12) {
-					code.append("0");
-				}
+	private static void getProvince() {
+		Document doc = open(SITE_URL);
+		Elements links = doc.select("tr.provincetr").select("a");
+		long startTimeStamp = System.currentTimeMillis();
+		for (int i = 0; i < links.size(); i++) {
+			Element e = links.get(i);
+			RegionEntry province = new RegionEntry();
+			String href = e.attr("href"); // 14.html
+			String[] arr = href.split("\\.");
+			String name = e.text();
+			StringBuffer code = new StringBuffer(arr[0]);
+			while (code.length() < 12) {
+				code.append("0");
+			}
 
-				province.setCode(code.toString());
-				province.setName(name);
+			province.setCode(code.toString());
+			province.setName(name);
+//			System.out.println(code + " == " + e.text());
+			String absHref = e.attr("abs:href");
+
+			// 创建一个新的线程
+			Thread t = new Thread(name) {
 				/**
 				 *               
 				 */
-				printSQL(name, code.toString(), null, writer);
-				System.out.println(code + " == " + e.text());
-				String absHref = e.attr("abs:href");
-				getCity(absHref, province, writer);
+				@Override
+				public void run() {
+					try {
+						File file = createFile(name);
+						PrintWriter writer = new PrintWriter(file);
+						printSQL(name, code.toString(), null, writer);
+						getCity(absHref, province, writer);
+						writer.close();
+						long endTimeStamp = System.currentTimeMillis();
+						System.out.println("********************" + name + " finish done! 程序共执行了：" + (endTimeStamp-startTimeStamp)*1.0/(1000*60) + "分钟");
+					} catch (FileNotFoundException e1) {
+						e1.printStackTrace();
+					}
+
+				}
+			};
+			t.start();
+		}
+	}
+
+	private static File createFile(String name) {
+		File file = new File("C:\\Users\\ZZY\\Desktop\\2016\\" + name + ".sql");
+		File dir = file.getParentFile();
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		try {
+			if (!file.exists()) {
+				file.createNewFile();
 			}
 
-		 
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
+		return file;
 	}
-	
+
 	/**
 	 * 获取市
+	 * 
 	 * @param url
 	 * @param province
 	 * @param writer
 	 */
 	private static void getCity(String url, RegionEntry province, PrintWriter writer) {
-		 Document doc = open(url);
-			Elements links = doc.select("tr.citytr");
-			RegionEntry city;
-			for (Element e : links) {
-				city = new RegionEntry();
-				Elements alist = e.select("a");
-				Element codeE = alist.get(0);
-				Element codeN = alist.get(1);
-				String name = codeN.text();
+		Document doc = open(url);
+		Elements links = doc.select("tr.citytr");
+		RegionEntry city;
+		for (Element e : links) {
+			city = new RegionEntry();
+			Elements alist = e.select("a");
+			Element codeE = alist.get(0);
+			Element codeN = alist.get(1);
+			String name = codeN.text();
 
-				String code = codeE.text();
+			String code = codeE.text();
 
-				// if ("市辖区".equals(name)) {
-				// name = province.getName();
-				// //code = region.getCode();
-				// }
+			/**
+			 *  
+			 */
+			printSQL(name, code, province.getCode(), writer);
+			// System.out.println(code + "==" + name);
+			city.setCode(code);
+			city.setName(name);
 
-				/**
-				 *  
-				 */
-				printSQL(name, code, province.getCode(), writer);
-				System.out.println(code + "==" + name);
-				city.setCode(code);
-				city.setName(name);
+			String absHref = codeE.attr("abs:href");
+			getCounty(absHref, city, writer);
+		}
 
-				String absHref = codeE.attr("abs:href");
-				getCounty(absHref, city, writer);
-			}
-
-		 
 	}
 
 	/**
 	 * 获取县
+	 * 
 	 * @param url
 	 * @param city
 	 * @param writer
@@ -127,7 +156,7 @@ public class JsoupTest {
 				 *  
 				 */
 				printSQL(name, code, city.getCode(), writer);
-				System.out.println(code + "==" + name);
+				// System.out.println(code + "==" + name);
 			} else {
 				alist = e.select("td");
 				String name = alist.get(1).text();
@@ -135,7 +164,7 @@ public class JsoupTest {
 				county.setCode(code);
 				county.setName(name);
 				printSQL(name, code, city.getCode(), writer);
-				System.out.println(alist.get(0).text() + "=***=" + alist.get(1).text());
+//				System.out.println(alist.get(0).text() + "=***=" + alist.get(1).text());
 			}
 
 		}
@@ -144,6 +173,7 @@ public class JsoupTest {
 
 	/**
 	 * 获取城镇
+	 * 
 	 * @param url
 	 * @param county
 	 */
@@ -164,7 +194,7 @@ public class JsoupTest {
 			 *  
 			 */
 			printSQL(name, code, county.getCode(), writer);
-			System.out.println(code + "==" + name);
+			// System.out.println(code + "==" + name);
 			town.setCode(code);
 			town.setName(name);
 
@@ -176,6 +206,7 @@ public class JsoupTest {
 
 	/**
 	 * 获取乡村
+	 * 
 	 * @param absHref
 	 * @param county
 	 */
@@ -195,7 +226,7 @@ public class JsoupTest {
 			 *  
 			 */
 			printSQL(name, code, town.getCode(), writer);
-			System.out.println(code + "==" + name);
+			// System.out.println(code + "==" + name);
 			village.setCode(code);
 			village.setName(name);
 
@@ -222,8 +253,10 @@ public class JsoupTest {
 		}
 		return doc;
 	}
+
 	/**
 	 * 输出SQL语句
+	 * 
 	 * @param name
 	 * @param code
 	 * @param parentCode
@@ -236,4 +269,14 @@ public class JsoupTest {
 		writer.println(sb.toString());
 	}
 
+	private static PrintWriter getWriter() {
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("C:\\Users\\ZZY\\Desktop\\data.sql");
+			return writer;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
